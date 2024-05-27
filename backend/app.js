f6bc3f6a160459809883f667e61a7292a0686678
LahiruWanigasooriya <lahirunisaluor123@gmail.com>
2024-05-27 15:27:26 +0530

    cloudinary created (blogController.js started)

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ import cookieParser from 'cookie-parser';
 import { dbConnection } from './database/dbConnection.js';
 import { errorMiddleware } from './middlewares/error.js';
 import userRouter from './routes/userRouter.js';
+import fileUpload from 'express-fileupload';
 
 const app = express();
 dotenv.config({ path: './config/config.env' });
@@ -20,6 +21,11 @@ app.use(
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(fileUpload({
+    useTempFiles: true,
+    tempFileDir: '/tmp/',
+}));
 
 app.use('/api/v1', userRouter);
 
