44c4fd428fe1031e8887aa57fb3a77db023c600e
LahiruWanigasooriya <lahirunisaluor123@gmail.com>
2024-06-19 21:46:51 +0530

    app.js update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,41 @@
 import React from 'react';
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+import Blogs from "./components/pages/Blogs";
+import SingleBlog from "./components/pages/SingleBlog";
+//import Navbar from "../src/components/layout/Navbar";
+//import Footer from "../src/components/layout/Footer";
+//import { Toaster } from "react-hot-toast";
+import Dashboard from "./components/pages/Dashboard";
+import Register from "./components/pages/Register";
+import Login from "./components/pages/Login";
+import AllAuthors from "./components/pages/AllAuthors";
+//import { Context } from "./main";
+//import axios from "axios";
+import UpdateBlog from "./components/pages/UpdateBlog";
 
 const App = () => {
-  return <></>;
+  return (
+    <>
+
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/blogs" element={<Blogs />} />
+          <Route path="/blog/:id" element={<SingleBlog />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/authors" element={<AllAuthors />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/blog/update/:id" element={<UpdateBlog />} />
+        </Routes>
+      </Router>
+
+    </>
+  );
 };
 
 export default App
