c60c689dc04b599c9bd450ae36de06e55e3a7e18
LahiruWanigasooriya <lahirunisaluor123@gmail.com>
2024-07-15 00:30:49 +0530

    Update latest blog

--- frontend/src/components/miniComponents/LatestBlogs.jsx
+++ frontend/src/components/miniComponents/LatestBlogs.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { Context } from "../../main";
+import { Link } from "react-router-dom";
 
-const LatestBlog = () => {
+const LatestBlogs = ({ heading, newClass }) => {
+  const { blogs } = useContext(Context);
+
   return (
-    <div>LatestBlog</div>
-  )
-}
+    <section
+      className={
+        newClass && newClass.length > 0 ? "dashboard-blogs blogs" : "blogs"
+      }
+    >
+      <h3>{heading}</h3>
+      <div className="container">
+        {blogs &&
+          blogs.map((element) => (
+            <Link to={`/blog/${element._id}`} className="card" key={element._id}>
+              <img src={element.mainImage.url} alt="blog" />
+              <span className="category">{element.category}</span>
+              <h4>{element.title}</h4>
+              <div className="writer_section">
+                <div className="author">
+                  <img src={element.authorAvatar} alt="author_avatar" />
+                  <p>{element.authorName}</p>
+                </div>
+              </div>
+            </Link>
+          ))}
+      </div>
+    </section>
+  );
+};
 
-export default LatestBlog
+export default LatestBlogs;
