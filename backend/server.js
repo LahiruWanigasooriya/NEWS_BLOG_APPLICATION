753b1c67f1cbfe1475b490c330c5d3d52b8d1535
Lahiru <lahirunisaluor123@gmail.com>
2025-12-31 12:25:18 +0530

    prepare backend for Vercel

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,8 @@ cloudinary.v2.config({
     api_secret: process.env.CLOUDINARY_CLIENT_SECRET,
 });
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server running on port ${process.env.PORT}`);
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
